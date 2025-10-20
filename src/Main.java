a8a70f7879fe389edeb043ad67674132d8066e1c
Mihaly Cozmuta Andrei <andrei.mihaly@stud.ubbcluj.ro>
2025-10-20 23:12:37 +0300

    function for the cheapest keyboard

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
+        int[] keyboards={40, 35, 70, 15, 45};
+        int[] usbs={20,15,40,15};
+
+        //1
+        System.out.println("Electronic shop");
+        System.out.print("The prices of the keyboards are: ");
+        for(int keyboard:keyboards){
+            System.out.print(keyboard+" ");
+        }
+        System.out.print("\n"+"The price of the cheapest keyboard is: "+eShop.cheapestItem(keyboards));
 
     }
 }
--- src/eShop.java
+++ src/eShop.java
@@ -1,3 +1,11 @@
 public class eShop {
-
+    public static int cheapestItem(int[] items){
+        int min=Integer.MAX_VALUE;
+        for (int item:items){
+            if(item<min){
+                min=item;
+            }
+        }
+        return min;
+    }
 }
